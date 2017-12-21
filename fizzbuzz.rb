8d35609e0d5141ba0201ea7c2002d991a41d7aee
Veronica Kim <vero.a.kim@gmail.com>
2017-12-21 00:52:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(x)
-  if x % 3 ==  0
+  if x % 3 == 0 && x % 5 == 0
+  return "FizzBuzz"
+elsif x % 3 ==  0
     return "Fizz"
   elsif x % 5 == 0
     return "Buzz"
-  elsif x % 3 == 0 && x % 5 == 0
-    return "FizzBuzz"
-  else
-    return "nil"
+
+  
 end
