e9af91f7976ee885225cf7a91d456a1583bb01f4
Veronica Kim <vero.a.kim@gmail.com>
2017-12-21 00:40:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(n)
-  if n / 3 == 1
+  if n / 3 == % 0
     return "Fizz"
-  elsif n / 5 == 1
+  elsif n / 5 == % 0
     return "Buzz"
-  elsif n / 15 == 1
+  elsif n / 15 == % 0
     return "FizzBuzz"
   else
     return "nil"
